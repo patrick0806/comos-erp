48c9417b1ec46413d9f7287d6077223fd38d8bde
Patrick da Silva Nicezi <patrickk0806@gmail.com>
2022-07-11 19:54:24 -0300

    get user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-  @Post('')
+  @Get(':email')
+  async findUser(@Param('email') email: string) {
+    return this.usersService.findUser(email);
+  }
+
+  @Post()
   async createSecureServer(@Body() newUser: UserDTO) {
     return this.usersService.createUser(newUser);
   }
